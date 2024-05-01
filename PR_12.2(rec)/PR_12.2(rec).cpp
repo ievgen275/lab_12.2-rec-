ec05b6d1cdf1305fa2fa8405c55f056a7c46c0a8
ievgen275 <ievgen3435@gmail.com>
2024-05-01 21:57:03 +0300

    first commit

---
+++ PR_12.2(rec)/PR_12.2(rec).cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+typedef int Info;
+struct Elem {
+    Elem* link;
+    Info info;
+};
+
+Elem* p = nullptr;
+
+void appendRecursive(Elem*& current, Info newData) {
+    if (current == nullptr) {
+        current = new Elem();
+        current->info = newData;
+        current->link = nullptr;
+        return;
+    }
+    appendRecursive(current->link, newData);
+}
+
+void increaseListValuesRecursive(Elem* current, Info increaseBy) {
+    if (current != nullptr) {
+        current->info += increaseBy;
+        increaseListValuesRecursive(current->link, increaseBy);
+    }
+}
+
+void printListRecursive(Elem* current) {
+    if (current != nullptr) {
+        cout << current->info << " ";
+        printListRecursive(current->link);
+    }
+    else {
+        cout << endl;
+    }
+}
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    appendRecursive(p, 1);
+    appendRecursive(p, 2);
+    appendRecursive(p, 3);
+    appendRecursive(p, 4);
+    appendRecursive(p, 5);
+
+    cout << "Початковий список: ";
+    printListRecursive(p);
+
+    Info increaseBy;
+    cout << "Введіть на скільки збільшити значення кожного елемента: ";
+    cin >> increaseBy;
+
+    increaseListValuesRecursive(p, increaseBy);
+
+    cout << "Змінений список: ";
+    printListRecursive(p);
+
+    return 0;
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,27 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../PR_12.2(rec)/PR_12.2(rec).cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+        TEST_METHOD(TestAppendRecursive)
+        {
+            Info testData1 = 10;
+            Info testData2 = 20;
+            Info testData3 = 30;
+
+            appendRecursive(p, testData1);
+            appendRecursive(p, testData2);
+            appendRecursive(p, testData3);
+
+            Assert::AreEqual(testData1, p->info);
+            Assert::AreEqual(testData2, p->link->info);
+            Assert::AreEqual(testData3, p->link->link->info);
+        }
+	};
+}
